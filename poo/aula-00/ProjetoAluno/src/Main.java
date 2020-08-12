aa1bceb4fe47af00fdb8af3814484c83d86411f2
Brenno Natal <natalbrenno@gmail.com>
2020-08-12 17:09:14 -0300

    Implementing constructor method

--- poo/aula-00/ProjetoAluno/src/Main.java
+++ poo/aula-00/ProjetoAluno/src/Main.java
@@ -2,13 +2,7 @@
 public class Main {
     public static void main(String[] args) {
         // gerando um  objeto --> variavel --> tipo (obrigatoriamente uma classe)
-        Aluno aluno1 = new Aluno();
-
-        aluno1.nome = "Brenno";
-        aluno1.curso = "TECH";
-        aluno1.ra = 114476;
-        aluno1.nota1 = 9.5;
-        aluno1.nota2 = 9.8;
+        Aluno aluno1 = new Aluno(114476, "Brenno", "TECH", 9.5, 10);
 
         System.out.println(aluno1);
     }
